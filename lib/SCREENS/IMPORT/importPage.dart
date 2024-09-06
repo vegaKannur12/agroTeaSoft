aec8eb2b3454d66d3ac0f825b3fc4fe51400cbb2
vegaKannur12 <100991705+vegaKannur12@users.noreply.github.com>
2024-09-06 17:00:07 +0530

    sep 6ev

--- lib/SCREENS/IMPORT/importPage.dart
+++ lib/SCREENS/IMPORT/importPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tsupply/SCREENS/DRAWER/customdrawer.dart';
 
 class ImportPage extends StatefulWidget {
   const ImportPage({super.key});
@@ -10,6 +11,13 @@ class ImportPage extends StatefulWidget {
 class _ImportPageState extends State<ImportPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,     
+      ),
+      drawer: CustomDrawer(),
+      body: SingleChildScrollView(),
+    );
   }
 }
